4792e2d0e122efc8444a8ec6b5c77970c7a9119b
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-07-15 16:21:11 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,38 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+  get '/recipes' do
+    erb :index
+  end
+
+  get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes' do
+    erb :index
+  end
+
+  get '/recipes/:id' do
+    params[:id]
+    erb :recipe
+  end
+
+  get '/recipes/:id/edit' do
+    params[:id]
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+    erb :recipe
+  end
+
+  post '/recipes/:id/delete' do
+    erb :delete
+  end
+
+  delete '/recipes/:id' do
+    redirect '/index'
+  end
 
 end
